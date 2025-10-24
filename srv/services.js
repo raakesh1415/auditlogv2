9369ee9a04150a70accd2b04f2de25951070e33d
Raakesh AL Ovannan <ralovannan@ppg.com>
2025-10-24 11:33:22 +0000

    update uplod to gdrive

--- srv/services.js
+++ srv/services.js
@@ -2,13 +2,15 @@ const processReqHeader = require('./lib/processReqHeader');
 const retrieveAuditLogs = require('./lib/retrieveAuditLogs');
 const convertToText = require('./lib/convertToText');
 const sendEmail = require('./lib/sendEmail');
+const uploadToDrive = require('./lib/uploadToDrive');
 
 module.exports = (srv) => {
     srv.on('retrieveAuditLogs', async (req) => {
         // const isJob = await processReqHeader(req);
         const auditLogData = await retrieveAuditLogs();
-        const textFilePath = await convertToText(auditLogData);
-        const result = await sendEmail(textFilePath);
+        const textFilePaths = await convertToText(auditLogData);
+        const driveUpload = await uploadToDrive(textFilePaths);
+        const result = await sendEmail(textFilePaths);
         
         return result;
     });
